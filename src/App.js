b63b81d71c33cc77ae5b113b6dc5c14f49ff37ec
Macas <marczakmaciej2@gmail.com>
2020-04-19 22:17:05 +0200

    add click

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Person from "./Person/Person";
 
-function App() {
-	return (
-		<div className="App">
-			<h1>Hi, I'm a React App</h1>
-			<p>This is really working!</p>
-			<Person name="Max" age="28" />
-			<Person name="Waldek" age="21" birthday="21.04.2019">
-				My Hobbies: Racing
-			</Person>
-			<Person name="Maria" age="22" />
-		</div>
-	);
+class App extends Component {
+	state = {
+		persons: [
+			{
+				name: "Max",
+				age: 28,
+			},
+			{ name: " Marry", age: 21 },
+
+			{ name: " Waldek", age: 20 },
+		],
+	};
+	switchNameHandler = () => {
+		console.log("Was Clicked!");
+	};
+	render() {
+		return (
+			<div className="App">
+				<h1>Hi, I'm a React App</h1>
+				<p>This is really working!</p>
+				<button onClick={this.switchNameHandler}>Switch Name</button>
+				<Person
+					name={this.state.persons[0].name}
+					age={this.state.persons[0].age}
+				/>
+				<Person
+					name={this.state.persons[1].name}
+					age={this.state.persons[1].age}
+				>
+					My Hobbies: Racing
+				</Person>
+				<Person
+					name={this.state.persons[2].name}
+					age={this.state.persons[2].age}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
