d5df01ff315e453fae1d5d88759eba3b7d119512
Macas <marczakmaciej2@gmail.com>
2020-04-19 22:02:24 +0200

    props/children

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import Person from "./Person/Person";
 function App() {
 	return (
 		<div className="App">
-			<Person></Person>
+			<h1>Hi, I'm a React App</h1>
+			<p>This is really working!</p>
+			<Person name="Max" age="28" />
+			<Person name="Waldek" age="21" birthday="21.04.2019">
+				My Hobbies: Racing
+			</Person>
+			<Person name="Maria" age="22" />
 		</div>
 	);
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const Person = () => {
+const Person = props => {
 	return (
 		<div>
-			<p>I'am Person!</p>
+			<p>
+				I'am {props.name} and I am {props.age} years old and birthday{" "}
+				{props.birthday}
+			</p>
+			<p>{props.children}</p>
 		</div>
 	);
 };
