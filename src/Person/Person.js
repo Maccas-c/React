5a6425d61961934da19f7b842c6500c95c90910b
Macas <marczakmaciej2@gmail.com>
2020-04-22 22:02:18 +0200

    add styled componenets

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,23 @@
 import React from "react";
 import "./Person.css";
 import Male from "../Person/Male";
+
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+	width: 60%;
+	margin: auto;
+	border: 1px solid gray;
+	box-shadow: 0 2px 3px rgb(102, 87, 87);
+	padding: 16px;
+	text-align: center;
+	@media (min-width: 500px) {
+		width: 450px;
+	}
+`;
 const Person = props => {
 	return (
-		<div className="Person">
+		<StyledDiv>
 			<p onClick={props.click}>
 				I'am {props.name} and I am {props.age} years old and birthday:
 				{props.birthday}
@@ -11,7 +25,7 @@ const Person = props => {
 			</p>
 			<p>{props.children}</p>
 			<input type="text" onChange={props.changed} value={props.name} />
-		</div>
+		</StyledDiv>
 	);
 };
 
